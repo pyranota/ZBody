4b3f6b5db4590932ed996ca3435c47e495166b25
pyranota <pyra@duck.com>
2024-07-17 23:02:12 +0200

    Work on `zb-core`

--- zb-core/src/main.zig
+++ zb-core/src/main.zig
@@ -1,20 +1,29 @@
 const std = @import("std");
 const engine = @import("./engine.zig");
 const tree = @import("./tree.zig");
+const Body = @import("body.zig");
 
 pub fn main() !void {
     // var tr = tree.Tree().new();
     // tr.addBody(81, .{ .x = 0, .y = 1 });
     // // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     // std.debug.print("{?}\n", .{tr});
-    var tr = try tree.Tree().init(16);
-    defer tr.deinit();
+    // var tr = try tree.Tree().init(16);
+    // defer tr.deinit();
 
-    try tr.addBody(1, .{ .x = 1, .y = 9 });
-    try tr.addBody(8, .{ .x = 0, .y = 1 });
-    try tr.addBody(10, .{ .x = 10, .y = 0 });
-    try tr.addBody(81, .{ .x = 9, .y = 11 });
-    try tr.print();
+    // try tr.addBody(1, .{ .x = 1, .y = 9 });
+    // try tr.addBody(8, .{ .x = 0, .y = 1 });
+    // try tr.addBody(10, .{ .x = 10, .y = 0 });
+    // try tr.addBody(81, .{ .x = 9, .y = 11 });
+    // try tr.print();
+    var e = try engine.Engine().init(256);
+    try e.addBody(Body{ .mass = 99, .position = .{ .x = 199, .y = 18 }, .velocity = .{} });
+    try e.addBody(Body{ .mass = 9, .position = .{ .x = 99, .y = 8 }, .velocity = .{} });
+    try e.addBody(Body{ .mass = 16, .position = .{ .x = 99, .y = 99 }, .velocity = .{} });
+    try e.step(0.1);
+    // try e.print();
+    e.tree.traverse();
+    defer e.deinit();
 }
 
 test "simple test" {
