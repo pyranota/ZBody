30c395cf4194a9c47769bfb02a6068c41d3a2f0f
pyranota <pyra@duck.com>
2024-07-17 23:01:54 +0200

    Implement traversing and write tests.
    This is pretty big commit.
    In general it implements traversing mechanisms, also fixes bug. 
    Now its using allocators.
    Also basic test suit

--- zb-core/src/tree.zig
+++ zb-core/src/tree.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const Vec2 = @import("vec2.zig");
+const Vec2 = @import("vec2.zig").Vec2;
 const alloc = std.heap.page_allocator;
 
 pub fn Tree() type {
@@ -60,6 +60,9 @@ pub fn Tree() type {
                 self.* = Node{ .branch = branch };
 
                 // TODO: Safetychecks on 0
+                if (leaf.size == 1) {
+                    return;
+                }
                 leaf.size /= 2;
 
                 // Ask a new branch where to put leaf
@@ -93,12 +96,30 @@ pub fn Tree() type {
             }
 
             /// Find out in which quadrant should we put node with given position
+            /// Can return error if position is out of bound
             fn which(self: @This(), position: Vec2) u2 {
                 const half = self.size() / 2;
 
                 const x: u2 = if (position.x < half) 0 else 1;
                 const y: u2 = if (position.y < half) 0 else 1;
                 return (x + y * 2);
+            }
+
+            /// Show what are spatial coordinates of child
+            fn where(self: @This(), quadrant: u2) Vec2 {
+                //
+                const i = quadrant;
+                const s = self.size() / 2;
+
+                // Reference implementation from Venx:
+                // UVec3::new(i & 1, (i >> 1) & 1,
+                var v = Vec2{ //
+                    .x = i & 1,
+                    .y = (i >> 1) & 1,
+                };
+                v.x *= s;
+                v.y *= s;
+                return v;
             }
         };
 
@@ -116,7 +137,10 @@ pub fn Tree() type {
         size: u32,
 
         /// Create new QuadTree
-        pub fn init(size: u32) !Self {
+        pub fn init(comptime size: u32) !Self {
+            if (!((size & (size - 1)) == 0)) {
+                @compileError("Bad value, size should be a power of two.");
+            }
             return .{ .size = size };
         }
         /// Deinit QuadTree
@@ -130,11 +154,27 @@ pub fn Tree() type {
 
         /// Add Astronomical Body to the System
         pub fn addBody(self: *@This(), mass: u32, position: Vec2) !void {
+            if (position.max() >= self.size) {
+                return ErrorError.Abc;
+            }
             try Tree().visitNode(&self.root, mass, position, self.size);
         }
 
         fn visitNode(node: *?*Node, mass: u32, position: Vec2, size: u32) !void {
             if (node.*) |n| {
+                // There is already node on the given spot
+                // TODO: Throw runtime error or warning
+                // TODO: refactor
+                switch (n.*) {
+                    // We dont care if its branch,
+                    // Cuz if it is, than there is more than one leaf and we cannot determine if position is actually the same
+                    .leaf => |leaf| {
+                        if (position.x == leaf.position.x and position.y == leaf.position.y) {
+                            return;
+                        }
+                    },
+                    else => {},
+                }
 
                 // In *which* *quadrant* do we want to put this node
                 const quadrant = n.which(position);
@@ -216,53 +256,116 @@ pub fn Tree() type {
             return e + b;
         }
 
-        fn traverse(self: Self, callback: fn (i32) i32) void {
-            _ = self; // autofix
-            _ = callback(99);
-            _ = callback(9);
+        pub fn traverse(self: Self) void {
+            Self.visitNodeTraverse(@constCast(&self.root), .{}, cb);
+        }
 
-            // TODO
+        fn cb(node: *Node, position: Vec2) void {
+            switch (node.*) {
+                .leaf => |leaf| {
+                    _ = leaf; // autofix
+
+                    std.debug.print("Leaf Position: {?} \n", .{position});
+                },
+                .branch => |branch| {
+                    _ = branch; // autofix
+                    std.debug.print("Branch Position: {?} \n", .{node.branch.mass});
+                },
+            }
+        }
+
+        fn visitNodeTraverse(node: *?*Node, position: Vec2, callback: fn (*Node, Vec2) void) void {
+            if (node.*) |n| {
+                switch (n.*) {
+                    .leaf => |leaf| {
+                        var p = position;
+                        p.x += leaf.position.x;
+                        p.y += leaf.position.y;
+                        cb(n, p);
+                    },
+                    .branch => |branch| {
+                        for (branch.children, 0..) |child, quadrant| {
+                            var qPosition = n.where(@intCast(quadrant));
+                            // std.debug.print("qPos: {?}", .{qPosition});
+                            qPosition.x += position.x;
+                            qPosition.y += position.y;
+
+                            Self.visitNodeTraverse(@constCast(&child), qPosition, callback);
+                        }
+                    },
+                }
+            } else {}
         }
     };
 }
+// TODO: Move in other module
+const ErrorError = error{
+    Abc,
+};
 
 const tt = std.testing;
 
 test "init tree test" {
-    var tr = Tree().init(16);
-    tr.addBody(81, .{ .x = 0, .y = 1 });
-
-    // std.debug.print("{?}\n\n", .{tr});
-    try tr.print();
-
-    // Expected tree
-    // var exTr = Tree().init();
-
-    // exTr.root = @constCast(&Tree().Node.newLeaf(81, .{ .x = 0, .y = 1 }));
-
-    // try tt.expectEqualDeep(exTr, tr);
+    // TODO
 }
+
 test "init tree, 2 bodies test" {
-    var tr = Tree().init(16);
-    tr.addBody(81, .{ .x = 0, .y = 1 });
-    tr.addBody(81, .{ .x = 8, .y = 8 });
-    tr.addBody(81, .{ .x = 10, .y = 0 });
-    tr.addBody(81, .{ .x = 0, .y = 10 });
+    // TODO
+}
 
-    // std.debug.print("{?}", .{tr});
-    // try pretty.print(alloc, tr, .{});
+test "init tree with wrong size" {
+    // Right sizes
+    _ = try Tree().init(16);
+    _ = try Tree().init(8);
+    _ = try Tree().init(64);
+    _ = try Tree().init(1024);
 
-    // Expected tree
-    // try tt.expect(false);
-    try tt.expect(tr.root != null);
-    try tt.expect(tr.root.?.branch.children[0] != null);
-    // try tt.expect(tr.root.?.branch.children[3] != null);
-    // for (0..4) |i| {
-    std.debug.print("{any}\n", .{tr.root.?.branch.children});
-    // }
-    // var exTr = Tree().init();
+    // Wrong size
+    // Cant test it, becauze it raises compile time error if size is wrong
+    // In order to run tests, we need to compile first
+    //
+    // Uncomment next line to test if it fails to build
+    // You should get this error: Bad value, size should be a power of two.
+    _ = try Tree().init(99);
+}
 
-    // exTr.root = @constCast(&Tree().Node.newLeaf(81, .{ .x = 0, .y = 1 }));
+test "add body with same position" {
+    var tr = try Tree().init(64);
 
-    // try tt.expectEqualDeep(exTr, tr);
+    try tr.addBody(11, .{ .x = 0, .y = 1 });
+    try tr.addBody(12, .{ .x = 8, .y = 8 });
+    try tr.addBody(13, .{ .x = 10, .y = 0 });
+    try tr.addBody(14, .{ .x = 0, .y = 10 });
+
+    var tr2 = try Tree().init(64);
+
+    try tr2.addBody(11, .{ .x = 0, .y = 1 });
+    try tr2.addBody(12, .{ .x = 8, .y = 8 });
+    try tr2.addBody(13, .{ .x = 10, .y = 0 });
+    try tr2.addBody(14, .{ .x = 0, .y = 10 });
+    // Add the same body
+    try tr2.addBody(14, .{ .x = 0, .y = 10 });
+
+    try tt.expectEqualDeep(tr, tr2);
+}
+
+test "traverse leafs and check positions" {
+    var tr = try Tree().init(64);
+
+    try tr.addBody(11, .{ .x = 0, .y = 1 });
+    try tr.addBody(12, .{ .x = 8, .y = 8 });
+    try tr.addBody(13, .{ .x = 10, .y = 0 });
+    try tr.addBody(14, .{ .x = 0, .y = 10 });
+
+    // TODO: Do actual testing
+    // Rn just watch the output and compare with values above
+    tr.traverse();
+}
+
+test "Add node outside the bound" {
+    std.debug.print("New test starting\n", .{});
+    var tr = try Tree().init(16);
+
+    // Outside of 16
+    try tt.expectError(ErrorError.Abc, tr.addBody(11, .{ .x = 0, .y = 22 }));
 }
