0534d17c1aaf4256e9a02a3f64122a9f0b1013a9
pyranota <pyra@duck.com>
2024-07-23 12:30:56 +0200

    Fallback version with flake commented out all dependencies

--- flake.nix
+++ flake.nix
@@ -6,13 +6,14 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
+  outputs =
+    { nixpkgs
+    , flake-utils
+    , ...
+    }:
     flake-utils.lib.eachDefaultSystem (
-      system: let
+      system:
+      let
         pkgs = nixpkgs.legacyPackages.${system};
         nativeBuildInputs = with pkgs; [
           zig
@@ -20,113 +21,101 @@
         ];
         buildInputs = with pkgs; [
           raylib
-            # libxcursor-dev
+          # libxcursor-dev
           xorg.libXcursor
           xorg.libX11
           xorg.libXrandr
           xorg.libXinerama
           xorg.libXi
-          xorg.libX11
-           mesa
-          alsa-lib
-          xorg.libX11.dev # adding the dev doesn't make a difference as far as I can tell
-          xorg.libXft
-          xorg.libXinerama
-          xorg.xorgproto
-          libGL
-          libGLU
-          binutils stdenv wget stdenv.cc raylib glfw
-          xorg.libX11.dev
-          openssl
-          pkg-config
-          pkg-config
-          libGL
-          xorg.libXft
-          libatomic_ops
-          mesa
-          alsa-lib
-          glibc
-          wayland
-          wayland-protocols
-          libxkbcommon
-          glfw-wayland
-          glfw
-
-          
+          # xorg.libX11
+          # mesa
+          # alsa-lib
+          # xorg.libX11.dev # adding the dev doesn't make a difference as far as I can tell
+          # xorg.libXft
+          # xorg.libXinerama
+          # xorg.xorgproto
+          # libGL
+          # libGLU
+          # binutils
+          # stdenv
+          # wget
+          # stdenv.cc
+          # raylib
+          # glfw
+          # xorg.libX11.dev
+          # openssl
+          # pkg-config
+          # pkg-config
+          # libGL
+          # xorg.libXft
+          # libatomic_ops
+          # mesa
+          # alsa-lib
+          # glibc
+          # wayland
+          # wayland-protocols
+          # libxkbcommon
+          # glfw-wayland
+          # glfw
         ];
-      in {
+      in
+      {
         devShells.default = pkgs.mkShell {
-          inherit nativeBuildInputs buildInputs; 
+          inherit nativeBuildInputs buildInputs;
           shellHook = ''onefetch; echo "Type Just to Run"'';
-          packages = with pkgs; [
-            raylib
-          ];
 
+          LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
+            xorg.libX11
+            xorg.libXi
+          ];
         };
         packages.default = pkgs.stdenv.mkDerivation {
           name = "Z-body";
           src = ./.;
-            packages = with pkgs; [
-                rustup
-                cmake
-                clang
-                pkg-config
-                wayland
-                glfw
-          raylib
-            # libxcursor-dev
-          xorg.libXcursor
-          xorg.libX11
-          xorg.libXrandr
-          xorg.libXinerama
-          xorg.libXi
-          xorg.libX11
-           mesa
-          alsa-lib
-          xorg.libX11.dev # adding the dev doesn't make a difference as far as I can tell
-          xorg.libXft
-          xorg.libXinerama
-          xorg.xorgproto
-          libGL
-          libGLU
-          binutils stdenv wget stdenv.cc raylib glfw
-          xorg.libX11.dev
-          openssl
-          pkg-config
-          pkg-config
-          libGL
-          xorg.libXft
-          libatomic_ops
-          mesa
-          alsa-lib
-          glibc
-          wayland
-          wayland-protocols
-          libxkbcommon
-          glfw-wayland
-          glfw
 
-          
-        ];
+          buildInputs = with pkgs; [
+            wayland
+            glfw
+            libGL
+            raylib-games
+            glfw-wayland
+            wayland-protocols
+          ] ++ (with xorg; [
+            libXrandr
+            libXinerama
+            libXcursor
+            libXi
+          ]) ++ buildInputs;
 
-          inherit buildInputs;
+          # packages = with pkgs; [
+          #   xorg.libX11
+          #   xorg.libX11.dev
+          #   xorg.libXi
+          #   wayland
+          #   glfw-wayland
+          #   glfw
+          #   libGL
+          #   raylib-games
+          #   glfw-wayland
+          #   wayland-protocols
+          # ];
 
-            LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
-              libGL
-              xorg.libXrandr
-              xorg.libXinerama
-              xorg.libXcursor
-              xorg.libXi
-            ];
-          LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
 
-          shellHook = ''onefetch; echo "ust to Run"'';
-          # postPatch = ''
-          #     # ln -s ${zb-core} $ZIG_GLOBAL_CACHE_DIR/p
-          #     ln -s ${zb-core} $out
-          # '';
-          postPatch = ''
-          '';
+          runtimeDependencies = with pkgs.xorg; [
+            libX11
+            libXi
+          ];
+
+          propagatedBuildInputs = with pkgs.xorg; [
+            libX11
+            libXi
+          ];
+
+          nativeBuildInputs = with pkgs; [
+            xorg.libX11
+            xorg.libXi
+            makeWrapper
+          ];
 
           buildPhase = ''
             mkdir -p $out/.cache/zig/p
@@ -135,32 +124,19 @@
             ${pkgs.zig}/bin/zig build --global-cache-dir $out/.cache/zig
           '';
 
+          # dontUnpack = true; # don't unpack the dependencies
+          LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
+            xorg.libX11
+            xorg.libXi
+          ];
           installPhase = ''
             ${pkgs.zig}/bin/zig build --global-cache-dir $out/.cache/zig install --prefix $out
             rm -rf $out/.cache # remove cache
-            # cp $src/zig-out/bin/Z-body $out 
-            # mkdir $out/bin
-            # mkdir -p $out/bin
-
-            # touch $out/bin/zbody
-          '';
+            # wrapProgram $out/bin/Z-body --set LD_LIBRARY_PATH ${pkgs.lib.makeLibraryPath [ pkgs.xorg.libX11 pkgs.xorg.libXi ]}:$LD_LIBRARY_PATH
+            wrapProgram $out/bin/Z-body --prefix \
+            'exec ${pkgs.nix}/bin/nix shell -c "LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ pkgs.xorg.libX11 pkgs.xorg.libXi ]}:$LD_LIBRARY_PATH exec $0 $@"'
+            '';
         };
-
-        # packages.default = pkgs.stdenv.mkDerivation {
-        #   pname = "template";
-        #   version = "0.0.0";
-        #   src = ./.;
-
-        #   nativeBuildInputs =
-        #     nativeBuildInputs
-        #     ++ [
-        #       pkgs.zig.hook
-
-        #       pkgs.raylib
-        #     ];
-        #   inherit buildInputs;
-        # };
-        # packages.default = pkgs.hello;
       }
     );
 }
