b25cb21995b5d390567265f19f8210b76a17dfcb
Dbaz <d.baz16767@gmail.com>
2024-07-29 14:14:11 +0200

    tree debug works on locked body

--- src/lock.zig
+++ src/lock.zig
@@ -15,20 +15,24 @@ const isDebugBounds = &@import("debug.zig").isDebugBounds;
 
 var isTargetModeOn: bool = false;
 var targetBodyId: u32 = undefined;
-var targetBody: core.Body = undefined;
+pub var targetBody: core.Body = undefined;
 var engine = &@import("main.zig").engine;
 pub var isLocked = false;
-
 /// Allows you to "lock" camera on body and spectate it.
 pub fn handleLock() void {
     mapKeys();
     if (isTargetModeOn) {
         for (engine.bodies.items) |body| {
-            const pos = rl.getScreenToWorld2D(rl.getMousePosition(), camera.*);
+            const pos = rl.getScreenToWorld2D(rl.getMousePosition(), ctrl.camera);
             const bodyVec = rl.Vector2{ .x = body.position[0], .y = body.position[1] };
 
             // A bit embeded debug here.
             if (isDebugBounds.*) {
+                const body_p = rl.Vector2.init(body.position[0], body.position[1]);
+                const scr_coords = rl.getWorldToScreen2D(body_p, ctrl.camera);
+
+                if (scr_coords.x > 980 or scr_coords.y > 980 or scr_coords.y < 20 or scr_coords.x < 20)
+                    continue;
                 rl.drawCircle(@intFromFloat(pos.x), @intFromFloat(pos.y), 10, rl.Color.white);
                 rl.drawCircle(@intFromFloat(bodyVec.x), @intFromFloat(bodyVec.y), body.radius * 1.25, rl.Color.white);
             }
@@ -43,17 +47,22 @@ pub fn handleLock() void {
             }
             if (isLocked and targetBodyId == body.id) {
                 targetBody = body;
-                player.x = rl.math.lerp(player.x, targetBody.position[0], 0.4);
-                player.y = rl.math.lerp(player.y, targetBody.position[1], 0.4);
+                player.x = targetBody.position[0];
+                player.y = targetBody.position[1];
+                ctrl.camera.target.x = targetBody.position[0];
+                ctrl.camera.target.y = targetBody.position[1];
             }
         }
     }
 }
 
 fn mapKeys() void {
-    if (rl.isKeyPressed(rl.KeyboardKey.key_l))
-        if (isPause.*) {
+    if (isPause.*) {
+        if (rl.isKeyPressed(rl.KeyboardKey.key_l)) {
             if (isTargetModeOn) isLocked = false;
             isTargetModeOn = !isTargetModeOn;
-        };
+        }
+    } else {
+        isTargetModeOn = false;
+    }
 }
