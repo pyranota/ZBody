e98afdd7fa67eb1ddfb3f98ea5b512856d23fc45
pyranota <pyra@duck.com>
2024-07-30 19:23:58 +0200

    Refactor `calcForcesCB`

--- zb-core/src/callbacks.zig
+++ zb-core/src/callbacks.zig
@@ -8,6 +8,7 @@
 const vec2 = @import("vec2.zig");
 const Vec2 = vec2.Vec2;
 const Vec2F = vec2.Vec2F;
+
 const TreeError = @import("error.zig").TreeError;
 const tree = @import("tree.zig");
 const Node = @import("node.zig").Node;
@@ -22,46 +23,73 @@ pub const stepArgs = struct {
     bodyMass: u32,
 };
 
+/// Calculate forces for given body
 pub fn calcForcesCB(node: *Node, nodePosition: Vec2, args: stepArgs) bool {
+    // -------------- Tracy ----------------//
     const zone = ztracy.Zone(@src());
     defer zone.End();
+    // -------------------------------------//
 
+    // m2
     const mass: f32 = switch (node.*) {
         inline else => |case| case.mass,
     };
 
+    // Spatial size of branch or leaf
     const size: f32 = @floatFromInt(node.size());
 
     // Can be position of body within leaf
     // Or center of mass in branch
-    const container_position = switch (node.*) {
-        .leaf => |leaf| leaf.position,
-        .branch => |branch| branch.centerOfMass,
-    };
+    const container_position = node.coordinates();
+
+    // Tracy
     const accelerationCalcZone = ztracy.ZoneN(@src(), "vector calculation zone");
 
+    // Node position (f32)
     const np = vec2.convert(f32, nodePosition);
     // Global position
     const global_position = container_position + np;
-
     // Direction from our node and target
-    const d_vec: Vec2F = global_position - args.bodyPos;
-
+    // No normalisation applied
+    const dir: Vec2F = global_position - args.bodyPos;
     // Distance
     const vec_zero: Vec2F = @splat(0);
-    const d = vec2.distance(f32, vec_zero, d_vec);
+    const r = vec2.distance(f32, vec_zero, dir);
 
+    // Tracy
     accelerationCalcZone.End();
 
     // Dont calculate force if target is the same as node
-    if (@floor(d) == 0)
+    if (@floor(r) == 0)
+        // TODO: May be return false?
         return true;
-    if (node.* == Node.leaf or size / d < Threshold) {
+
+    if (
+    // We reached the buttom of tree
+    node.* == Node.leaf
+    // We are far enough to treat this branch as a single body
+    or size / r < Threshold) {
+        // Tracy
         const applyAccel = ztracy.ZoneN(@src(), "Apply accleration zone");
-        const accel: Vec2F = @splat((mass) / (d * d * d + Safety));
-        const G: Vec2F = @splat(1);
-        args.accel.* += accel * d_vec * G;
+
+        // F = m1 * m2 / r^2;
+        // F = m1 * a;
+        // a = F / m1;
+        //
+        // [ a = m2 / r^2 ]
+        //
+        //
+        //      Padding, we dont want inf accel by dividing by 0. >----<
+        const accel: Vec2F = @splat((mass) / (r * r * r + Safety));
+        //                                                    ^ Extra r, because we are supposed to multiply by
+        //  normalized directional vector, so we multiply by another r.
+        //                     >---< Here, that is not normalized vector.
+        args.accel.* += accel * dir;
+
+        // Tracy
         applyAccel.End();
+
+        // Drop branch from traversing
         return false;
     }
     return true;
